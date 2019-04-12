2e47c2bb019829bb74085861172a82107c1926f2
mark-37 <ansariazeem37@gmail.com>
2019-04-12 21:45:42 +0530

    Java Provisioner file added

--- mean.sh
+++ mean.sh
@@ -62,22 +62,3 @@ fi
 
 mkdir workspace
 echo "########### WORKSPACE CREATED ###########"
-
-#installing Oracle Java 11
-
-JAVA_INSTALLED=$(dpkg-query -W --showformat='${Status}\n' $JAVA | grep "install ok installed")
-echo "Checking for $JAVA: $JAVA_INSTALLED"
-if [ "" == "$JAVA_INSTALLED" ]; then
-
-    echo oracle-java11-installer shared/accepted-oracle-license-v1-2 select true | sudo /usr/bin/debconf-set-selections
-    echo oracle-java11-installer shared/accepted-oracle-licence-v1-2 boolean true | sudo /usr/bin/debconf-set-selections
-
-    echo "deb http://ppa.launchpad.net/linuxuprising/java/ubuntu bionic main" | tee /etc/apt/sources.list.d/linuxuprising-java.list
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 73C3DB2A
-    apt-get update
-    apt-get install -y oracle-java11-installer oracle-java11-set-default
-
-
-fi
-
-echo "Everying set and done..!"
